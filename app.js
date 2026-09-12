53644105c7dcb7dc9d13964d4b795b459c583083
samirvithlani <samir.vithlani83955@gmail.com>
2026-09-12 17:56:02 +0530

    express strats

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-console.log("app file loaded..")
+const express = require("express")
+//refereance variable crearte..
+const app = express()
 
-// const user = require("./users")
-// console.log(user) // {}
-// console.log(user.userName)
-// console.log(user.userAge)
 
-const {userName,userAge} = require("./users")
-console.log(userName)
-console.log(userAge)
 
-const employee = require("./employee")
-console.log(employee) //{Function}
-var ans = employee.setEmployee(100)
-console.log("ans = ",ans)
 
-var x = employee.getFullName("virat","kohli")
-console.log(x)
+const PORT = 3000
+
+//server creation.
+app.listen(PORT,()=>{
+    console.log(`server started on port ${PORT}`)
+})
--- users.js
+++
@@ -1,9 +0,0 @@
-console.log("user file loaded..")
-var userName = "amit"
-var userAge = 23
-module.exports = {
-    userName,userAge
-}
-// module.exports = userName
-// module.exports = userAge
-
