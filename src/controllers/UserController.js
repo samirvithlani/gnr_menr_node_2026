1b70ce0d05854726930ee51b9c165d8403b4b255
samirvithlani <samir.vithlani83955@gmail.com>
2026-09-22 18:35:41 +0530

    user by id

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,25 +1,35 @@
-const userModel = require("../models/UserModel")
+const userModel = require("../models/UserModel");
 
-const getAllUsers = async(req,res)=>{
+const getAllUsers = async (req, res) => {
+  const users = await userModel.find(); //[]
 
-    const users = await userModel.find() //[]
-    
+  res.json({
+    message: "user fetched",
+    data: users,
+  });
+};
+
+const getUserById = async (req, res) => {
+  const id = req.params.id;
+
+  //db.users.find({_id:id})
+  //db.users.findOne({_id:id})
+  //const foundUser = await userModel.findOne({_id:id})
+  const foundUser = await userModel.findById(id);
+  if (foundUser) {
     res.json({
-        message:"user fetched",
-        data:users
-    })
-
-}
-
-const getUserById = (req,res)=>{
-    const id = req.params.id
+      message: "user found",
+      data: foundUser,
+    });
+  }
+  else{
     res.json({
-        message:"user by id",
-        data:id
+        message:"user not found"
     })
-}
+  }
+};
 
-module.exports ={
- getAllUsers,
- getUserById   
-}
+module.exports = {
+  getAllUsers,
+  getUserById,
+};
